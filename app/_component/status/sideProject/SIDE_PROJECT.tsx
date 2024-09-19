790b75d692e44d7a64d2513ba5ab9c5771a69528
zer0 <maelcolome.pro@icloud.com>
2024-09-19 16:49:40 +0200

    :lipstick: Change text

--- app/_component/status/sideProject/SIDE_PROJECT.tsx
+++ app/_component/status/sideProject/SIDE_PROJECT.tsx
@@ -5,25 +5,25 @@ export const SIDE_PROJECTS: SideProjectType[] = [
   {
     Logo: UtensilsCrossed,
     title: "Crazee-Burger",
-    description: "Projet de plateforme type Burger-King. techno (react, firebase, styled-components, react-router)",
+    description: "Project: Burger King-like platform Tech stack: React, Firebase, Styled-components, React Router",
     url: "https://crazeeburger-zer0absolute-zer0s-projects-da2820b3.vercel.app/?_vercel_share=WjpJDbyeeNwZEyjK00J8Umm7pM3j4jQJ",
   },
   {
     Logo: GraduationCap,
-    title: "Mon Poulpe",
-    description: "Projet de mise en place d’une plateforme pour apprendre les bases de différentes technos.",
+    title: "In-comming",
+    description: "Project: Setting up a platform to learn the basics of different technologies.",
     url: "/https://monpoulpe.fr",
   },
   {
     Logo: ScrollText,
     title: "In-comming",
-    description: "Site web de cuisine où vous pourrez retrouver des recettes (macros) pour tous types de régimes.",
+    description: "Cooking website where you can find recipes (macros) for all types of diets.",
     url: "/",
   },
   {
     Logo: Notebook,
     title: "In-comming",
-    description: "Création d’une application web de prise de notes via la voix, avec une intégration d’IA pour apporter du style à la note.",
+    description: "Creation of a web application for voice note-taking, with AI integration to enhance the style of the notes.",
     url: "/",
   },
 ]
